851fd9747fb154bd23b1d28c3bbb5e58764d8908
Gizcan <77542968+Gizcan@users.noreply.github.com>
2021-01-16 16:32:32 +0000

    Add files via upload

---
+++ _rnek1.ino
@@ -0,0 +1,13 @@
+//43. Pine bağlı ledi yakan programı yazacağız.
+
+void setup() //Başlangıç tanımlamaları
+{ 
+  pinMode(43,OUTPUT);   //43 Numaralı pini çıkış olarak ayarladık
+
+}
+
+void loop() //Ana kodun olduğu yer
+{
+  digitalWrite(43,HIGH);  //43 Numarayı yaktık
+
+}
